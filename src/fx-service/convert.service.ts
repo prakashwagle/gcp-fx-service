cda414a90f23c1091a1a99cf88d085dfdc239628
Prakash Wagle <prakashwagle@live.in>
2021-02-13 18:06:15 -0500

    fix build bugs

--- src/fx-service/convert.service.ts
+++ src/fx-service/convert.service.ts
@@ -16,4 +16,3 @@ public getDiscription(ticker:string):string{
     }
 }
 }
-  
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,13 @@ import express from "express";
 import path from "path";
 import * as http from 'http';
 import {json} from "body-parser";
-import { ConvertRoutesConfig } from "./fx-service/convert.routes.config";
 import debug from "debug";
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port: Number = 4000;
-const routes: ConvertRoutesConfig[] = [];
+const port: number = 4000;
 const debugLog: debug.IDebugger = debug('app');
 
 app.use(json());
-app.use(ConvertRoutesConfig);
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
